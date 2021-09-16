820eefdd0456663634111ddf1772fece6a3e2609
Naufal Aulia <naufal7600@gmail.com>
2021-09-17 02:37:11 +0700

    add enemies

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class LevelManager : MonoBehaviour
 {
@@ -23,11 +25,59 @@ public class LevelManager : MonoBehaviour
     [SerializeField] private GameObject towerUIPrefab;
     
     [SerializeField] private Tower[] towerPrefabs;
+    [SerializeField] private Enemy[] enemyPrefabs;
 
+    [SerializeField] private Transform[] enemyPaths;
+    [SerializeField] private float spawnDelay = 5f;
+
+    
     private List<Tower> _spawnedTowers = new List<Tower>();
+    [SerializeField] private List<Enemy> _spawnedEnemies = new List<Enemy>();
+    
+    private float _runningSpawnDelay;
+
     private void Start()
     {
         InstantiateAllTowerUI();
+    }
+
+    private void Update()
+    {
+        // every <spawnDelay> seconds spawn enemy
+        _runningSpawnDelay -= Time.unscaledDeltaTime;
+        if (_runningSpawnDelay <= 0f)
+        {
+            SpawnEnemy();
+            _runningSpawnDelay = spawnDelay;
+        }
+
+        // move each enemy if is active
+        foreach (Enemy enemy in _spawnedEnemies)
+        {
+            if (!enemy.gameObject.activeSelf)
+            {
+                continue;
+            }
+
+            if (Vector2.Distance(enemy.transform.position, enemy.TargetPosition) < 0.1f)
+            {
+                Debug.Log(Vector2.Distance(enemy.transform.position, enemy.TargetPosition));
+                enemy.SetCurrentPathIndex(enemy.CurrentPathIndex+1);
+                if (enemy.CurrentPathIndex < enemyPaths.Length)
+                {
+                    enemy.SetTargetPosition(enemyPaths[enemy.CurrentPathIndex].position);
+                }
+                else
+                {
+                    enemy.gameObject.SetActive(false);
+                }
+            }
+            else
+            {
+                enemy.MoveToTarget();
+            }
+        }
+
     }
 
     private void InstantiateAllTowerUI()
@@ -45,5 +95,40 @@ public class LevelManager : MonoBehaviour
     public void RegisterSpawnedTower(Tower tower)
     {
         _spawnedTowers.Add(tower);
+    }
+
+    private void SpawnEnemy()
+    {
+        int randomIndex = Random.Range(0, enemyPrefabs.Length);
+        string enemyIndexString = (randomIndex + 1).ToString();
+
+        GameObject newEnemyObj = _spawnedEnemies.Find(
+            e => !e.gameObject.activeSelf && e.name.Contains(enemyIndexString)
+        )?.gameObject;
+
+        if (newEnemyObj == null)
+        {
+            newEnemyObj = Instantiate(enemyPrefabs[randomIndex].gameObject);
+        }
+
+        Enemy newEnemy = newEnemyObj.GetComponent<Enemy>();
+        if (!_spawnedEnemies.Contains(newEnemy))
+        {
+            _spawnedEnemies.Add(newEnemy);
+        }
+
+        newEnemy.transform.position = enemyPaths[0].position;
+        newEnemy.SetTargetPosition(enemyPaths[1].position);
+        newEnemy.SetCurrentPathIndex(1);
+        newEnemy.gameObject.SetActive(true);
+    }
+
+    private void OnDrawGizmos()
+    {
+        for (int i = 0; i < enemyPaths.Length-1; i++)
+        {
+            Gizmos.color = Color.cyan;
+            Gizmos.DrawLine(enemyPaths[i].position, enemyPaths[i+1].position);
+        }
     }
 }
